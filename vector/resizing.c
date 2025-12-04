2f47157a1fd1b5632544f8b6d628298b0b9776ae
vitorma28 <vimat28.3.1@gmail.com>
2025-12-03 22:15:14 -0300

    Feito o Push e Pop do Vector.

--- vector/resizing.c
+++ vector/resizing.c
@@ -1,14 +1,62 @@
 #include "private.h"
 #include "vector.h"
 
-
-static void VectorShift(Vector * vec) {}
-
-
-static void VectorUnshift(Vector * vec) {}
+#include <string.h>
 
 
-void VectorPush(Vector * vec, void * value) {}
+static Vector * VectorGrow(Vector * vec) {
+    void * newSpace = vec->allocate(vec->maxLength * 2, vec->moreContext);
+
+    if (newSpace == NULL) return NULL;
+
+    memcpy(newSpace, vec->content, vec->maxLength);
+
+    vec->liberate(vec->content, vec->moreContext);
+
+    vec->content = newSpace;
+    vec->maxLength *= 2;
+
+    return vec;
+}
+
+
+static void VectorShift1Right(Vector * vec, size_t index) {
+    if (index >= vec->length - 1) {
+        return;
+    }
+
+    for (size_t i = vec->length - 2; i >= index; i--) {
+        size_t offset = i * vec->elemSize;
+        size_t offsetNext = offset + vec->elemSize;
+        memcpy(vec->content + offsetNext, vec->content + offset, vec->elemSize);
+    }
+}
+
+
+static void VectorShift1Left(Vector * vec, size_t index) {
+    if (index >= vec->length - 1) {
+        return;
+    }
+
+    for (size_t i = index; i < vec->length - 1; i++) {
+        size_t offset = i * vec->elemSize;
+        size_t offsetNext = offset + vec->elemSize;
+        memcpy(vec->content + offset, vec->content + offsetNext, vec->elemSize);
+    }
+}
+
+
+void VectorPush(Vector * vec, void * value) {
+    if (vec->length == vec->maxLength) {
+        void * tmp = VectorGrow(vec);
+
+        if (tmp == NULL) return;
+    }
+
+    memcpy(vec->content + vec->length * vec->elemSize, value, vec->elemSize);
+
+    vec->length++;
+}
 
 
 void VectorPop(Vector * vec) {
